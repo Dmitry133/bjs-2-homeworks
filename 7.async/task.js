0e1f7e4f1135ec9c69b7f43ddea2eafc0365d87a
Dmitry Logachev <dmitrylog92@gmail.com>
2023-02-24 23:20:13 +0700

    help please

---
+++ 7.async/task.js
@@ -0,0 +1,59 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+
+    addClock(time, callback) {
+        let alarmTime = {
+            callback: callback,
+            time: time,
+            canCall: true,
+        };
+        
+        if (!time || !callback) {
+            throw new Error ("Отсутствуют обязательные аргументы");
+        } else if (this.alarmCollection.some(element => element.time === time)) {
+            console.warn('Уже присутствует звонок на это же время');
+        } else {
+            this.alarmCollection.push(alarmTime);
+        };
+    };
+
+    removeClock(time) {
+        this.alarmCollection = this.alarmCollection.filter(element => element.time !== time);
+    };
+   
+    getCurrentFormattedTime() {
+        return new Date().toLocaleString().slice(12, -3);
+    };
+
+    start() {
+        if (this.intervalId != null) {
+            return;
+        } else {
+            this.intervalId = setInterval(() => {
+                this.alarmCollection.forEach(element => {
+                    if (this.time === this.getCurrentFormattedTime()) {
+                        alarmTime.canCall = false;
+                        alarmTime.callback();
+                    };
+                });
+            }, 1000);
+        };    
+    };
+
+    stop() {
+       clearInterval(this.intervalId);
+       this.intervalId = null;
+    };
+
+    resetAllCalls() {
+        this.alarmCollection.forEach((element) => element.canCall = true);
+    };
+
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    };
+};
